a64f0e2b5c40426e085eba25ec952ea655a82160
Jack Hsieh <chengweih@google.com>
2024-06-28 15:35:55 -0700

    device connect state

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -11,12 +11,32 @@ import { CommonModule, NgIf } from '@angular/common';
 export class DeviceComponent implements OnInit {
   @Input() device: any; // Assuming 'device' is an object with a 'name' property
   deviceInfo: any; // To store the info from getInfo()  
+  isConnected = false; // Track connection state
+  hasConnectFeature = false; // Flag to check if 'connected' member exists
+
 
   async ngOnInit() { // Use ngOnInit lifecycle hook
     try {
       this.deviceInfo = await this.device.getInfo();
+      this.hasConnectFeature = typeof this.device.connect !== 'undefined';
+
+      if (this.hasConnectFeature) {
+        this.device.onconnect = this.onConnect;
+        this.device.onconnect = this.onDisconnect;
+        this.isConnected = this.device.connected;
+      }      
     } catch (error) {
       console.error('Error getting device info:', error);
     }
+  }  
+
+  onConnect() {
+    console.log('[DEBUG] device connect');
+    this.isConnected = this.device.connected;
+  }
+
+  onDisconnect() {
+    console.log('[DEBUG] device disconnect');
+    this.isConnected = this.device.connected;
   }  
 }
