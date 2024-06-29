8f02cc00adb06dbdc5e54796c9bda8681dc1f22f
Jack Hsieh <chengweih@chromium.org>
2024-06-28 20:40:25 -0700

    fix connected state

--- src/app/device/device.component.ts
+++ src/app/device/device.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule, NgIf } from '@angular/common';
+import {CommonModule, NgIf} from '@angular/common';
+import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-device',
@@ -9,34 +9,45 @@ import { CommonModule, NgIf } from '@angular/common';
   styleUrl: './device.component.css'
 })
 export class DeviceComponent implements OnInit {
-  @Input() device: any; // Assuming 'device' is an object with a 'name' property
-  deviceInfo: any; // To store the info from getInfo()  
-  isConnected = false; // Track connection state
-  hasConnectFeature = false; // Flag to check if 'connected' member exists
+  @Input()
+  device: any;      // Assuming 'device' is an object with a 'name' property
+  deviceInfo: any;  // To store the info from getInfo()
+  public isConnected = false;  // Track connection state
+  hasConnectFeature = false;   // Flag to check if 'connected' member exists
+
+  constructor(private cdRef: ChangeDetectorRef) {}
 
 
-  async ngOnInit() { // Use ngOnInit lifecycle hook
+  async ngOnInit() {  // Use ngOnInit lifecycle hook
     try {
       this.deviceInfo = await this.device.getInfo();
       this.hasConnectFeature = typeof this.device.connected !== 'undefined';
 
       if (this.hasConnectFeature) {
-        this.device.onconnect = this.onConnect.bind(this);
-        this.device.ondisconnect = this.onDisconnect.bind(this);
+        this.device.onconnect = this.onConnect;
+        this.device.ondisconnect = this.onDisconnect;
         this.isConnected = this.device.connected;
-      }      
+      }
     } catch (error) {
       console.error('Error getting device info:', error);
     }
-  }  
-
-  onConnect() {
-    console.log('[DEBUG] device connect');
-    this.isConnected = this.device.connected;
   }
 
-  onDisconnect() {
-    console.log('[DEBUG] device disconnect');
+  updateConnectState() {
     this.isConnected = this.device.connected;
-  }  
+    // Manuall trigger change detection because this might be called by
+    // asynchronous operations like as promise or a timeout.
+    this.cdRef.detectChanges();
+  }
+
+  onConnect =
+      () => {
+        console.log('[DEBUG] device connect');
+        this.updateConnectState();
+      }
+
+  onDisconnect = () => {
+    console.log('[DEBUG] device disconnect');
+    this.updateConnectState();
+  }
 }
